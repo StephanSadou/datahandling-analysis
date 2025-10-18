22597a70370a91e3f541c38dd570ef838db4c58e
FarheenRossaye <zuleikha.rossaye1@umail.uom.ac.mu>
2025-10-18 11:49:24 +0400

    updated field name for lagged data

--- SQL_Schema/3.0_JOINING FILES & CREATE VIEW.sql
+++ SQL_Schema/3.0_JOINING FILES & CREATE VIEW.sql
@@ -1,60 +1,12 @@
 #----------CREATE VIEW, JOIN TABLES & CREATE NEW FIELDS TO BE USED FOR ANALYSIS PURPOSES-----------
 USE data_handling;
-#1. View 1: Compile all datasets and align current harvest year with prior year climate data
 
-DROP VIEW lagged_compiled_data; #remove already existing view
-CREATE VIEW lagged_compiled_data AS
-SELECT 
-    cl.*,ag.Year as Harvest_Year,
-    Area_harvested,
-    Production,
-	Yield,
-    ei.value as GDP_Value
-FROM agriculture ag
-
-JOIN (
-    SELECT
-      YEAR(date) as Climate_Year ,  
-
-      # Temperature Grouping
-      AVG(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Temperature_in_degree_celsius END) AS Avg_Temperature_Plantation,
-      AVG(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Temperature_in_degree_celsius END) AS Avg_Temperature_Growth,
-      AVG(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Temperature_in_degree_celsius END) AS Avg_Temperature_Maturation,
-
-      # Humidity Grouping
-      AVG(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Humidity_percent END) AS Avg_Humidity_Plantation,
-      AVG(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Humidity_percent END) AS Avg_Humidity_Growth,
-      AVG(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Humidity_percent END) AS Avg_Humidity_Maturation,
-
-      # Solar Radiation Grouping
-      AVG(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Solar_radiation_kWh_m2 END) AS Solar_Radiation_Plantation,
-      AVG(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Solar_radiation_kWh_m2 END) AS Solar_Radiation_Growth,
-      AVG(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Solar_radiation_kWh_m2 END) AS Solar_Radiation_Maturation,
-
-      # Total Rainfall Grouping
-      SUM(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Precipitation_in_mm END) AS Total_Rainfall_Plantation,
-      SUM(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Precipitation_in_mm END) AS Total_Rainfall_Growth,
-      SUM(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Precipitation_in_mm END) AS Total_Rainfall_Maturation,
-
-      # Rainy Days Grouping
-      SUM(CASE WHEN (MONTH(date) BETWEEN 7 AND 9 AND Precipitation_in_mm > 1) THEN 1 ELSE 0 END) AS Total_rainy_days_Plantation,
-      SUM(CASE WHEN ((MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) AND Precipitation_in_mm > 1) THEN 1 ELSE 0 END) AS Total_rainy_days_Growth,
-      SUM(CASE WHEN (MONTH(date) BETWEEN 4 AND 6 AND Precipitation_in_mm > 1) THEN 1 ELSE 0 END) AS Total_rainy_days_Maturation
-
-    FROM climate  
-    GROUP BY Climate_Year
-) cl
-ON ag.Year = cl.Climate_Year + 1   # Align climate from previous year with harvest year
-JOIN economic_indicator ei 
-ON ei.Year = ag.Year;
-
-#select count(*) From lagged_compiled_data :- Crosscheck linecount:- should be 1 less than linecount for FAOSTAT Data
-
-#--------CREATING VIEW 2 with same harvest and climate year---------
+#--------CREATING VIEW 1 with same harvest and climate year---------
 DROP VIEW current_compiled_data; #remove already existing view
 CREATE VIEW current_compiled_data AS
 SELECT 
-    cl.*,ag.Year as Harvest_Year,
+    ag.Year as Harvest_Year,
+    cl.*,
     Area_harvested,
     Production,
 	Yield,
@@ -95,6 +47,57 @@ JOIN (
 ) cl
 ON ag.Year = cl.Climate_Year  # Align climate from current year with current harvest year
 JOIN economic_indicator ei 
-ON ei.Year = ag.Ycurrent_compiled_datacurrent_compiled_dataear;
+ON ei.Year = ag.Year;
 
-select count(*) From current_compiled_data #:- Crosscheck linecount:- should be same as FAOSTAT Data
+select count(*) From current_compiled_data ;#:- Crosscheck linecount:- should be same as FAOSTAT Data
+
+#2. View 2: Compile all datasets and align current harvest year with prior year climate data
+
+DROP VIEW lagged_compiled_data; #remove already existing view
+CREATE VIEW lagged_compiled_data AS
+SELECT 
+ag.Year as Harvest_Year,
+    cl.*,
+    Area_harvested,
+    Production,
+	Yield,
+    ei.value as GDP_Value
+FROM agriculture ag
+
+JOIN (
+    SELECT
+      YEAR(date) as Climate_Year ,  
+
+      # Temperature Grouping
+      AVG(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Temperature_in_degree_celsius END) AS Avg_Temperature_Plantation_lag1,
+      AVG(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Temperature_in_degree_celsius END) AS Avg_Temperature_Growth_lag1,
+      AVG(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Temperature_in_degree_celsius END) AS Avg_Temperature_Maturation_lag1,
+
+      # Humidity Grouping
+      AVG(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Humidity_percent END) AS Avg_Humidity_Plantation_lag1,
+      AVG(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Humidity_percent END) AS Avg_Humidity_Growth_lag1,
+      AVG(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Humidity_percent END) AS Avg_Humidity_Maturation_lag1,
+
+      # Solar Radiation Grouping
+      AVG(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Solar_radiation_kWh_m2 END) AS Solar_Radiation_Plantation_lag1,
+      AVG(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Solar_radiation_kWh_m2 END) AS Solar_Radiation_Growth_lag1,
+      AVG(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Solar_radiation_kWh_m2 END) AS Solar_Radiation_Maturation_lag1,
+
+      # Total Rainfall Grouping
+      SUM(CASE WHEN MONTH(date) BETWEEN 7 AND 9 THEN Precipitation_in_mm END) AS Total_Rainfall_Plantation_lag1,
+      SUM(CASE WHEN (MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) THEN Precipitation_in_mm END) AS Total_Rainfall_Growth_lag1,
+      SUM(CASE WHEN MONTH(date) BETWEEN 4 AND 6 THEN Precipitation_in_mm END) AS Total_Rainfall_Maturation_lag1,
+
+      # Rainy Days Grouping
+      SUM(CASE WHEN (MONTH(date) BETWEEN 7 AND 9 AND Precipitation_in_mm > 1) THEN 1 ELSE 0 END) AS Total_rainy_days_Plantation_lag1,
+      SUM(CASE WHEN ((MONTH(date) BETWEEN 10 AND 12 OR MONTH(date) BETWEEN 1 AND 3) AND Precipitation_in_mm > 1) THEN 1 ELSE 0 END) AS Total_rainy_days_Growth_lag1,
+      SUM(CASE WHEN (MONTH(date) BETWEEN 4 AND 6 AND Precipitation_in_mm > 1) THEN 1 ELSE 0 END) AS Total_rainy_days_Maturation_lag1
+
+    FROM climate  
+    GROUP BY Climate_Year
+) cl
+ON ag.Year = cl.Climate_Year + 1   # Align climate from previous year with harvest year
+JOIN economic_indicator ei 
+ON ei.Year = ag.Year;
+
+#select count(*) From lagged_compiled_data :- Crosscheck linecount:- should be 1 less than linecount for FAOSTAT Data
