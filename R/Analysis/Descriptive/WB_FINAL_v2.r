81326304f089c3588fb2dde97a0de75b1e6406e3
nhlalwenhlekhumalo-bit <nhlalwenhle.khumalo@umail.uom.ac.mu>
2025-10-29 16:31:50 +0200

    Update WB_FINAL_v2.r

--- R/Analysis/Descriptive/WB_FINAL_v2.r
+++ R/Analysis/Descriptive/WB_FINAL_v2.r
@@ -24,7 +24,7 @@ if (!require("lmtest")) install.packages("lmtest"); library("lmtest")
 # Read the environment file to obtain the database credentials 
 root <- find_root(has_file(".Renviron"))
 readRenviron(file.path(root, ".Renviron"))
-
+result_folder = file.path(predictive_folder, "results")
 # Use the credentials to connect to our local database 
 con <- dbConnect(
   RMariaDB::MariaDB(),
@@ -72,6 +72,7 @@ GDP <- ggplot(agro_long, aes(x = year, y = Value, color = Series, linetype = Ser
   theme(legend.position = "right")
 # --- Step 6: Display and Save ---
 print(GDP)
+
 ggsave("agriculture_gdp_trend_mauritius.png", plot = GDP, width = 8, height = 5, dpi = 300)
 
 
@@ -255,4 +256,5 @@ view(ag_gdp_trends_full)
 
 
 #======================-----------END OF GDP SCRIPT---------======================#
+
 
